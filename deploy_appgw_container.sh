296dfedf1309c6333f20b7c57c36b621f5528c67
Michael CK Lee <leebug@gmail.com>
2024-08-30 09:12:04 +1000

    Update deploy_appgw_container.sh

--- deploy_appgw_container.sh
+++ deploy_appgw_container.sh
@@ -6,15 +6,15 @@ VNET_NAME=$4
 ALB_SUBNET_NAME=$5 
 ALB_Version=$6
 
-echo "arguments ==> $1 $2 $3 $4 $5 $6"
-echo "saved data from arguments ==> $AKS_NAME $IDENTITY_RESOURCE_NAME $RESOURCE_GROUP $VNET_NAME $ALB_SUBNET_NAME $ALB_Version"
+echo "data from arguments ==> $AKS_NAME $IDENTITY_RESOURCE_NAME $RESOURCE_GROUP $VNET_NAME $ALB_SUBNET_NAME $ALB_Version"
+echo "\n\r"
 
 # Create a user managed identity for ALB controller and federate the identity as Workload identity to use in AKS cluster
 mcResourceGroup=$(az aks show --resource-group $RESOURCE_GROUP --name $AKS_NAME --query "nodeResourceGroup" -o tsv)
 mcResourceGroupId=$(az group show --name $mcResourceGroup --query id -o tsv)
 
 echo "Creating identity $IDENTITY_RESOURCE_NAME in resource group $RESOURCE_GROUP"
-#az identity create --resource-group $RESOURCE_GROUP --name $IDENTITY_RESOURCE_NAME
+az identity create --resource-group $RESOURCE_GROUP --name $IDENTITY_RESOURCE_NAME
 principalId="$(az identity show -g $RESOURCE_GROUP -n $IDENTITY_RESOURCE_NAME --query principalId -o tsv)"
 
 echo "Waiting 60 seconds to allow for replication of the identity..."
